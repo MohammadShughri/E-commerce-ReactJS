1a1e9374893d0b423ca8a06704b66d31fbc32b22
MohammadShughri <Mohammadws19@gmail.com>
2021-07-09 15:44:48 +0300

    Added API support

--- src/components/Products/Product_design/Product.jsx
+++ src/components/Products/Product_design/Product.jsx
@@ -1,28 +1,46 @@
-import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
-import { AddShoppingCart } from '@material-ui/icons';
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
 const Product = ({ product }) => {
   const classes = useStyles();
 
+  console.log(product);
+
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={product.image} title={product.name} />
+      <CardMedia
+        className={classes.media}
+        image={product.media.source}
+        title={product.name}
+      />
       <CardContent>
         <div className={classes.cardContent}>
-          <Typography gutterBottom variant="h5" gutterBottom>
+          <Typography gutterBottom variant="h5">
             {product.name}
           </Typography>
           <Typography gutterBottom variant="h5">
-            {product.price}
+            {product.price.formatted_with_symbol}
           </Typography>
         </div>
-        <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+        {/* This method is used to ensure that we will not have any <p> inside the description of the items*/}
+        <Typography
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          variant="body2"
+          color="textSecondary"
+        />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to Cart" >
+        <IconButton aria-label="Add to Cart">
           <AddShoppingCart />
         </IconButton>
       </CardActions>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,26 +4,7 @@ import { Grid } from "@material-ui/core";
 import Product from "./Product_design/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running shoes.",
-    price: "$150",
-    image:
-      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/7ed0855435194229a525aad6009a0497_9366/Superstar_Shoes_White_EG4958_01_standard.jpg",
-  },
-  {
-    id: 2,
-    name: "MacBook",
-    description: "Apple MacBook",
-    price: "$999",
-    image:
-      "https://www.apple.com/v/macbook-pro-16/c/images/meta/og__csakh451i0eq_large.png",
-  },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
 
   return (
