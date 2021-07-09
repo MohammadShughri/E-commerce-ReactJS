30b1e5f44e56c68cf0578c24a207bc1c41ea4bc7
MohammadShughri <Mohammadws19@gmail.com>
2021-07-09 14:47:52 +0300

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import Products from './components/Products/Products';
+import React from "react";
+
+// import Products from './components/Products/Products';
+// import Navbar from './components/Navbar/Navbar';
+// Rather than importing the specific location of the file we can import the methods that we need directly
+
+import { Products, Navbar } from "./components";
 
 const App = () => {
-    return (
-        <div>
-            <Products />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <Products />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,15 +2,33 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import Product from "./Product_design/Product";
+import useStyles from "./styles";
 
 const products = [
-  { id: 1, name: "Shoes", description: "Running shoes.", price: "$150", image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/7ed0855435194229a525aad6009a0497_9366/Superstar_Shoes_White_EG4958_01_standard.jpg'},
-  { id: 2, name: "MacBook", description: "Apple MacBook", price: "$999", image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mbp-spacegray-select-202011_GEO_TR?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1613672889000'},
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running shoes.",
+    price: "$150",
+    image:
+      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/7ed0855435194229a525aad6009a0497_9366/Superstar_Shoes_White_EG4958_01_standard.jpg",
+  },
+  {
+    id: 2,
+    name: "MacBook",
+    description: "Apple MacBook",
+    price: "$999",
+    image:
+      "https://www.apple.com/v/macbook-pro-16/c/images/meta/og__csakh451i0eq_large.png",
+  },
 ];
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
@@ -20,6 +38,6 @@ const Products = () => {
       </Grid>
     </main>
   );
-}
+};
 
 export default Products;
