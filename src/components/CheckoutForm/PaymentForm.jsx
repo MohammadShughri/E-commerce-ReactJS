34fb8b14db1c43aac60dd426461210717d98c1b4
MohammadShughri <Mohammadws19@gmail.com>
2021-07-11 20:05:17 +0300

    Added payment order details page

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Typography, Button, Divider } from "@material-ui/core";
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-const PaymentForm = () => {
-    return (
-        <div>
-            PaymentForm
-        </div>
-    )
-}
+import Review from "./Review";
 
-export default PaymentForm
+const PaymentForm = ({ checkoutToken }) => {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+    </>
+  );
+};
+
+export default PaymentForm;
